522129e015b6f42e0d2d75aba9d84cb8033d231f
arkadwera-ctrl <arkadwera@gmail.com>
2026-07-08 17:57:14 +0300

    lesson02

--- lesson02/lesson_2_task_2.py
+++ lesson02/lesson_2_task_2.py
@@ -1,6 +1,7 @@
 def is_year_leap(number):
     return True if number % 4 == 0 else False
 
-num = int(input("Год: "))
+num = int(input("введите номер года: "))
 result = is_year_leap(num)
-print(result)
+
+print(f"год {num}: {result}")
