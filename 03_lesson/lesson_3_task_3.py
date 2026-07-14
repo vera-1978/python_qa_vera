02d7533fa8b16509cb4c5db53a010da5ca7f4047
arkadwera-ctrl <arkadwera@gmail.com>
2026-07-14 21:14:18 +0300

    03_lesson

--- 03_lesson/lesson_3_task_3.py
+++ 03_lesson/lesson_3_task_3.py
@@ -5,7 +5,8 @@ to_address = Address("Россия", "Москва", "Тверская", 10, 5)
 from_address = Address("Россия", "Йошкар-Ола", "Советская", 45, 128)
 
 mailing = Mailing(to_address, from_address, 150.50, "RU123456789")
-
+print(f"Адрес получателя: {to_address}")
+print(f"Адрес отправителя: {from_address}")
 print(f"Отправление {mailing.track} из {mailing.from_address.city} "
       f"в {mailing.to_address.city}.")
 print(f"Стоимость: {mailing.cost} руб.")
