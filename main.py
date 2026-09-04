8d880f535dcba7bf84db6c03fc5ef6b3f467ba58
arkadwera-ctrl <arkadwera@gmail.com>
2026-09-04 22:26:26 +0300

    Remove .idea folder from GitHub

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные из файла .env
+load_dotenv()
+
+# Получаем значение переменной
+database_url = os.getenv("DATABASE_URL")
+
+print(f"Адрес базы данных: {database_url}")
